088a242471ccaa98c4f111d53fddcdaed8eb7b9e
His-Like <beitianpianye@gmail.com>
2025-11-20 06:48:28 +0900

    Update settings.h

--- dll/dll/settings.h
+++ dll/dll/settings.h
@@ -215,6 +215,7 @@ private:
     uint16 port{}; // Listen port, default 47584
 
     bool unlockAllDLCs = true;
+    bool unlockUINT32MAXCheck = false;
     std::vector<struct DLC_entry> DLCs{};
     
     //installed app ids, Steam_Apps::BIsAppInstalled()
@@ -421,6 +422,8 @@ public:
 
     //DLC stuff
     void unlockAllDLC(bool value);
+    void setUINT32MAX(bool value);
+    bool getUINT32MAX();
     void addDLC(AppId_t appID, std::string name, bool available);
     unsigned int DLCCount() const;
     bool hasDLC(AppId_t appID);
